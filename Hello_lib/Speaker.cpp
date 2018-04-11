7825f2302186c541156469d41fa8d077a159fd6a
Joan Massich <sik@visor.udg.edu>
2018-04-11 16:16:25 +0200

    Add hello world

---
+++ Hello_lib/Speaker.cpp
@@ -0,0 +1,12 @@
+#include "Speaker.h"
+
+#include <iomanip>
+#include <sstream>
+
+using namespace Hello;
+
+namespace Hello {
+  void Speaker::sayHello() {
+    std::cout << "Hello, world!\n";
+  }
+}
